1806c9333c2dc7d13c0b77a8b809dd51b0cbf001
Katie Dektar <kaytdek@gmail.com>
2015-11-15 09:47:09 -0800

    Refactor controller code out of ToggleFragment

---
+++ app/src/main/java/com/dektar/pi/piswitch/PiController.java
@@ -0,0 +1,119 @@
+package com.dektar.pi.piswitch;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.res.Resources;
+import android.preference.PreferenceManager;
+import android.util.Log;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+/**
+ * Class for communicating with and controlling the Pi.
+ */
+public class PiController {
+    public interface OnPiStatusResponseListener {
+        void onStatus(boolean isOn);
+        void onError();
+    }
+
+    private static final String HTTP_ON = "cgi-bin/on.py";
+    private static final String HTTP_OFF = "cgi-bin/off.py";
+    private static final String HTTP_STATUS = "status.php";
+
+    private static final String TAG = "request_tag";
+
+    private RequestQueue mRequestQueue;
+    private OnPiStatusResponseListener mStatusListener;
+    private String mUrlBase;
+
+    private boolean mIsInternalIp;
+    private String mInternalUrl;
+    private String mExternalUrl;
+
+    public PiController(Context context, OnPiStatusResponseListener listener) {
+        mStatusListener = listener;
+        loadPreferences(context);
+        mRequestQueue = Volley.newRequestQueue(context);
+    }
+
+    public void onDestroy() {
+        mRequestQueue.cancelAll(TAG);
+        mRequestQueue.stop();
+        mRequestQueue = null;
+    }
+
+    public void toggle(boolean turnOn) {
+        sendRequest(mUrlBase + "/" + (turnOn ? HTTP_ON : HTTP_OFF));
+    }
+
+    public void refreshAll(Context context) {
+        loadPreferences(context);
+        updateStatus();
+    }
+
+    public void updateStatus() {
+        String url = mUrlBase + "/" + HTTP_STATUS;
+
+        // Request a string response from the provided URL.
+        StringRequest statusRequest = new StringRequest(com.android.volley.Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        boolean isCurrentlyOn = parseResponse(response);
+                        mStatusListener.onStatus(isCurrentlyOn);
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                mStatusListener.onError();
+                Log.d("Error", error.getMessage());
+            }
+        });
+        statusRequest.setTag(TAG);
+        // Add the request to the RequestQueue.
+        mRequestQueue.add(statusRequest);
+    }
+
+    private void sendRequest(String url) {
+        StringRequest request = new StringRequest(com.android.volley.Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        updateStatus();
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                mStatusListener.onError();
+                Log.d("Error", ": " + error.getMessage());
+            }
+        });
+        request.setTag(TAG);
+        mRequestQueue.add(request);
+    }
+
+    private boolean parseResponse(String response) {
+        // TODO: Get JSON and parse that instead!
+        if (response.contains("\"lamps\":[{\"id\":\"1\",\"name\":\"living room\",\"state\":\"1\"}")) {
+            return true;
+        }
+        return false;
+    }
+
+    private void loadPreferences(Context context) {
+        SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(context);
+        Resources resources = context.getResources();
+        mIsInternalIp = sharedPref.getBoolean(resources.getString(R.string.pref_which_url_id),
+                true);
+        mInternalUrl = sharedPref.getString(resources.getString(R.string.pref_internal_url_id),
+                resources.getString(R.string.default_internal_url));
+        mExternalUrl = sharedPref.getString(resources.getString(R.string.pref_external_url_id),
+                resources.getString(R.string.default_external_url));
+        mUrlBase = mIsInternalIp ? mInternalUrl : mExternalUrl;
+    }
+}
--- app/src/main/java/com/dektar/pi/piswitch/ToggleFragment.java
+++ app/src/main/java/com/dektar/pi/piswitch/ToggleFragment.java
@@ -19,20 +19,11 @@ import com.android.volley.toolbox.Volley;
  * Toggle fragment.
  */
 public class ToggleFragment extends android.support.v4.app.Fragment {
-    private static final String HTTP_ON = "cgi-bin/on.py";
-    private static final String HTTP_OFF = "cgi-bin/off.py";
-    private static final String HTTP_STATUS = "status.php";
-
-    private static final String TAG = "request_tag";
-
     private ToggleButton toggleButton;
-    private boolean isCurrentlyOn;
-    private RequestQueue mRequestQueue;
-    private String mUrlBase;
+    private PiController mPiController;
+    private PiController.OnPiStatusResponseListener statusListener;
 
-    private Boolean mIsInternalIp;
-    private String mInternalUrl;
-    private String mExternalUrl;
+    private boolean mIsCurrentlyOn;
 
     private boolean mTryTurningOn = false;
     private boolean mTryTurningOff = false;
@@ -51,27 +42,39 @@ public class ToggleFragment extends android.support.v4.app.Fragment {
         // Inflate the layout for this fragment
         View rootView = inflater.inflate(R.layout.toggle_fragment, container, false);
 
-        loadPreferences();
-
         toggleButton = (ToggleButton) rootView.findViewById(R.id.toggle_button);
         toggleButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 resetButtonUi();
-                sendRequest(mUrlBase + "/" + (isCurrentlyOn ? HTTP_OFF : HTTP_ON));
+                mPiController.toggle(!mIsCurrentlyOn);
             }
         });
 
-        mRequestQueue = Volley.newRequestQueue(getActivity());
+        statusListener = new PiController.OnPiStatusResponseListener() {
+            @Override
+            public void onStatus(boolean isOn) {
+                mIsCurrentlyOn = isOn;
+                updateButtonState();
+            }
+
+            @Override
+            public void onError() {
+                resetButtonUi();
+            }
+        };
         resetButtonUi();
+
+        mPiController = new PiController(getActivity(), statusListener);
+
         if (mTryTurningOn) {
-            sendRequest(mUrlBase + "/" + HTTP_ON);
+            mPiController.toggle(true);
             mTryTurningOn = false;
         } else if (mTryTurningOff) {
-            sendRequest(mUrlBase + "/" + HTTP_OFF);
+            mPiController.toggle(false);
             mTryTurningOff = false;
         } else {
-            sendStatusRequest();
+            mPiController.updateStatus();
         }
 
         return rootView;
@@ -79,8 +82,7 @@ public class ToggleFragment extends android.support.v4.app.Fragment {
 
     @Override
     public void onResume() {
-        loadPreferences();
-        sendStatusRequest();
+        mPiController.refreshAll(getActivity());
         super.onResume();
     }
 
@@ -91,9 +93,7 @@ public class ToggleFragment extends android.support.v4.app.Fragment {
 
     @Override
     public void onDestroy() {
-        mRequestQueue.cancelAll(TAG);
-        mRequestQueue.stop();
-        mRequestQueue = null;
+        mPiController.onDestroy();
         super.onDestroy();
     }
 
@@ -105,7 +105,7 @@ public class ToggleFragment extends android.support.v4.app.Fragment {
 
     private void updateButtonState() {
         toggleButton.setEnabled(true);
-        if (isCurrentlyOn) {
+        if (mIsCurrentlyOn) {
             toggleButton.setText(getResources().getString(R.string.button_text_turn_off));
             toggleButton.setBackgroundColor(getResources().getColor(R.color.button_color_turn_off));
         } else {
@@ -113,67 +113,4 @@ public class ToggleFragment extends android.support.v4.app.Fragment {
             toggleButton.setBackgroundColor(getResources().getColor(R.color.button_color_turn_on));
         }
     }
-
-    private void sendStatusRequest() {
-        String url = mUrlBase + "/" + HTTP_STATUS;
-
-        // Request a string response from the provided URL.
-        StringRequest statusRequest = new StringRequest(com.android.volley.Request.Method.GET, url,
-                new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
-                        isCurrentlyOn = parseResponse(response);
-                        updateButtonState();
-                    }
-                }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                resetButtonUi();
-                Log.d("Error", error.getMessage());
-            }
-        });
-        statusRequest.setTag(TAG);
-        // Add the request to the RequestQueue.
-        mRequestQueue.add(statusRequest);
-    }
-
-    private void sendRequest(String url) {
-        StringRequest request = new StringRequest(com.android.volley.Request.Method.GET, url,
-                new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
-                        sendStatusRequest();
-                    }
-                }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                Log.d("Error", ": " + error.getMessage());
-            }
-        });
-        request.setTag(TAG);
-        mRequestQueue.add(request);
-    }
-
-    private boolean parseResponse(String response) {
-        // TODO: Get JSON and parse that instead!
-        if (response.contains("\"lamps\":[{\"id\":\"1\",\"name\":\"living room\",\"state\":\"1\"}")) {
-            return true;
-        }
-        return false;
-    }
-
-    private void loadPreferences() {
-        final SharedPreferences sharedPref =
-                PreferenceManager.getDefaultSharedPreferences(getActivity());
-        mIsInternalIp = sharedPref.getBoolean(
-                getResources().getString(R.string.pref_which_url_id), true);
-        mInternalUrl = sharedPref.getString(
-                getResources().getString(R.string.pref_internal_url_id),
-                getResources().getString(R.string.default_internal_url));
-        mExternalUrl = sharedPref.getString(
-                getResources().getString(R.string.pref_external_url_id),
-                getResources().getString(R.string.default_external_url));
-        mUrlBase = mIsInternalIp ? mInternalUrl : mExternalUrl;
-    }
-
 }
